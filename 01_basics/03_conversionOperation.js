5a09e70d709e08fa6e2509f1f53786548e17e2a4
lonevaibhav <singhvaibhav1943@gmail.com>
2025-06-19 06:11:12 +0000

    moreConversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,14 +1,16 @@
 let score="100";
 
 
-console.log(typeof(score)) ;
-console.log(typeof score);
+// console.log(typeof(score)) ;
+// console.log(typeof score);
 
 
 
 let valueInNumber=Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
 // 100=>number
 // "100abc"=>NaN
 // true=>1
@@ -17,10 +19,10 @@ console.log(valueInNumber);
 // undefined=>NaN
 // " "=>0
 
-let isLoggedIn= null;
-let valueInBoolean=Boolean(isLoggedIn) ;
-console.log(typeof valueInBoolean);
-console.log(valueInBoolean); 
+let value=true;
+let valueInBoolean=Boolean(value);
+// console.log(typeof valueInBoolean);
+// console.log(valueInBoolean); 
 
 
 
@@ -33,5 +35,53 @@ console.log(valueInBoolean);
 
 let someValue=123;
 let valueInString=String(someValue);
-console.log(typeof valueInString);
-console.log(valueInString); 
+// console.log(typeof valueInString);
+// console.log(valueInString); 
+
+
+// ***************************** operations *******************************
+
+let someNumber=5;
+let negValue=-someNumber;
+// console.log(negValue);
+
+// console.log(2 +2);
+// console.log(2 *2);
+// console.log(2 -2);
+// console.log(2 /2);
+// console.log(2 % 2);
+// console.log(2 ** 2); // exponentiation  
+// console.log(2 + "2"); // string concatenation
+// console.log(2 + "2");
+// console.log("2" + 2);
+// console.log( 1+ 2 + "3");
+ 
+let str1= "Hello";
+let str2= "Vaibhav";
+let str3= str1  +  str2;
+// console.log(str3);
+
+// console.log("1" +2);
+// console.log(1 + "2");
+// console.log(1 +"2" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2,num3
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter =100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);  
+
+
+
+
+
+
+
+
